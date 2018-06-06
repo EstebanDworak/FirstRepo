2a85dabfe57eb450d2bcaa3edb7dbb4fae26b337
EstebanDworak <mail@estebancervantes.com>
2018-06-06 12:28:19 -0500

    Update PI.c

--- PI.c
+++ PI.c
@@ -14,13 +14,14 @@
 #include <windows.h>
 
 
-#define MAX_THREADS 1
+#define THREADS 8
 long long mainCounter = 1;
-
+int i=0;
+long long precision = 5000000000;
 DWORD WINAPI calcular(double *suma);
 
 
-int main(void) {
+void main() {
 
 	setbuf(stdout,NULL);
 	fflush(stdin);
@@ -32,24 +33,15 @@ int main(void) {
 	clock_t start = clock();
 
 
+	HANDLE hThreadArray[THREADS];
 
-/*
-	HANDLE hThreadArray[MAX_THREADS];
-
-	for(int i=0;i<MAX_THREADS;i++){
+	for(i=0;i<THREADS;i++){
 		 hThreadArray[i] = CreateThread(NULL, 0, (void *)calcular, &sum, 0, NULL);
 	}
-
-    WaitForMultipleObjects(MAX_THREADS, hThreadArray, TRUE, INFINITE);
-    */
-
+	//WaitForSingleObject(hThreadArray[0], INFINITE);
+    WaitForMultipleObjects(THREADS, hThreadArray, TRUE, INFINITE);
 
 
-
-	for(long i =1; i<3000000000;i+=2){
-		sum+=(1.0)/(2*i-1);
-		sum-=(1.0)/(2*(i+1)-1);
-	}
 
 
 
@@ -66,13 +58,26 @@ int main(void) {
 
 
 DWORD WINAPI calcular(double *suma){
-	while(mainCounter<300000000){
-		*suma+=(1.0)/(2*mainCounter-1);
-		*suma-=(1.0)/(2*(mainCounter+1)-1);
-		mainCounter+=2;
+	long long n1= precision/THREADS*(long long)(i-1);
+	long long n2= precision/THREADS*(long long)i;
+
+	printf("\n%lli A %lli",n1,n2);
+
+	for(long long i =n1+1 ; i<n2;i+=2){
+		*suma+=(1.0)/(2*i-1);
+		*suma-=(1.0)/(2*(i+1)-1);
 	}
-	printf("\nDONE\n");
 
 }
 
+/*
+ *
+ *
 
+	for(long i =1; i<10000000;i+=2){
+		sum+=(1.0)/(2*i-1);
+		sum-=(1.0)/(2*(i+1)-1);
+	}
+
+ *
+ * */
